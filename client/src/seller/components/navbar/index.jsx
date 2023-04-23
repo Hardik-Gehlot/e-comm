aadf1f2d3f87d279a66cbb1a921fde0970acc03b
ankit kumar <ankitkr8084@gmail.com>
2023-04-23 10:16:20 +0530

    created Navbar

--- client/src/seller/components/navbar/index.jsx
+++ client/src/seller/components/navbar/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import "./style.scss"
+import React from "react";
+import "./style.scss";
+import logo from "/logo.svg";
+import { FaRegUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 function index() {
+  const navigate = useNavigate();
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <header className={`header`}>
+      <div className="logo" onClick={() => navigate("/")}>
+        <img src={logo} alt="E-com" />
+      </div>
+
+      <ul className="menuItems">
+        <li className="menuItem">Products</li>
+        <li className="menuItem">Orders</li>
+      </ul>
+
+      <div className="profile">
+        <FaRegUser></FaRegUser>
+      </div>
+    </header>
+  );
 }
 
-export default index
+export default index;
